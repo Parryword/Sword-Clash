58b0142c62b9f0796b0683c0e3d481e58b5e90d5
Yiğit Efe İren <yigitefeiren@gmail.com>
2025-07-26 08:55:27 +0300

    bugfix(tower): fixed arrow target selection

--- Assets/Scripts/TowerScript.cs
+++ Assets/Scripts/TowerScript.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TowerScript : MonoBehaviour
-{   
+{
     public Player player;
     public int deltaTime = 5;
     public GameObject arrowPrefab;
@@ -11,27 +12,43 @@ public class TowerScript : MonoBehaviour
     public Target target;
 
     private float timer = 0f;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        
+
         if (timer >= deltaTime)
         {
-            var spawnPosition = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y + 5);
-            if (Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) < range)
+            var closestEnemy = GameObject.FindGameObjectsWithTag("Enemy")
+                .OrderBy(e => Mathf.Abs(e.transform.position.x - transform.position.x))
+                .FirstOrDefault();
+
+            var spawnPosition = new Vector2(transform.position.x, transform.position.y + 5);
+
+            var withinRange = false;
+
+            if (target == Target.Player)
+            {
+                withinRange = Mathf.Abs(player.transform.position.x - transform.position.x) < range;
+            }
+            else if (closestEnemy != null) // check if we found any enemy
+            {
+                withinRange = Mathf.Abs(closestEnemy.transform.position.x - transform.position.x) < range;
+            }
+
+            if (withinRange)
             {
                 var arrow = Instantiate(arrowPrefab, spawnPosition, Quaternion.identity);
                 var arrowScript = arrow.GetComponent<ArrowScript>();
                 arrowScript.target = target;
             }
+
             timer = 0f;
         }
     }
